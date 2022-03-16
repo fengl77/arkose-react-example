1550e95fca743130c52ab8d8418263ad47071843
Mike Daley <michael_daley@icloud.com>
2022-03-16 10:40:22 +0000

    Latest

--- src/Components/CallbackStatus.js
+++ src/Components/CallbackStatus.js
@@ -1,70 +1,70 @@
-import React from "react";
+import React from 'react';
 
 const CallbackStatus = (props) => {
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <p className="col-2">onReady</p>
+    <div className='container'>
+      <div className='row justify-content-center'>
+        <p className='col-2'>onReady</p>
         {props.onReady ? (
-          <p className="col-1">✅</p>
+          <p className='col-1'>✅</p>
         ) : (
-          <p className="col-1"></p>
+          <p className='col-1'></p>
         )}
       </div>
-      <div className="row justify-content-center">
-        <p className="col-2">onShow</p>
-        {props.onShow ? <p className="col-1">✅</p> : <p className="col-1"></p>}
+      <div className='row justify-content-center'>
+        <p className='col-2'>onShow</p>
+        {props.onShow ? <p className='col-1'>✅</p> : <p className='col-1'></p>}
       </div>
-      <div className="row justify-content-center">
-        <p className="col-2">onShown</p>
+      <div className='row justify-content-center'>
+        <p className='col-2'>onShown</p>
         {props.onShown ? (
-          <p className="col-1">✅</p>
+          <p className='col-1'>✅</p>
         ) : (
-          <p className="col-1"></p>
+          <p className='col-1'></p>
         )}
       </div>
-      <div className="row justify-content-center">
-        <p className="col-2">onSuppress</p>
+      <div className='row justify-content-center'>
+        <p className='col-2'>onSuppress</p>
         {props.onSuppress ? (
-          <p className="col-1">✅</p>
+          <p className='col-1'>✅</p>
         ) : (
-          <p className="col-1"></p>
+          <p className='col-1'></p>
         )}
       </div>
-      <div className="row justify-content-center">
-        <p className="col-2">onCompleted</p>
+      <div className='row justify-content-center'>
+        <p className='col-2'>onCompleted</p>
         {props.onCompleted ? (
-          <p className="col-1">✅</p>
+          <p className='col-1'>✅</p>
         ) : (
-          <p className="col-1"></p>
+          <p className='col-1'></p>
         )}
       </div>
-      <div className="row justify-content-center">
-        <p className="col-2">onHide</p>
-        {props.onHide ? <p className="col-1">✅</p> : <p className="col-1"></p>}
+      <div className='row justify-content-center'>
+        <p className='col-2'>onHide</p>
+        {props.onHide ? <p className='col-1'>✅</p> : <p className='col-1'></p>}
       </div>
-      <div className="row justify-content-center">
-        <p className="col-2">onReset</p>
+      <div className='row justify-content-center'>
+        <p className='col-2'>onReset</p>
         {props.onReset ? (
-          <p className="col-1">✅</p>
+          <p className='col-1'>✅</p>
         ) : (
-          <p className="col-1"></p>
+          <p className='col-1'></p>
         )}
       </div>
-      <div className="row justify-content-center">
-        <p className="col-2">onError</p>
+      <div className='row justify-content-center'>
+        <p className='col-2'>onError</p>
         {props.onError ? (
-          <p className="col-1">✅</p>
+          <p className='col-1'>✅</p>
         ) : (
-          <p className="col-1"></p>
+          <p className='col-1'></p>
         )}
       </div>
-      <div className="row justify-content-center">
-        <p className="col-2">onFailed</p>
+      <div className='row justify-content-center'>
+        <p className='col-2'>onFailed</p>
         {props.onFailed ? (
-          <p className="col-1">✅</p>
+          <p className='col-1'>✅</p>
         ) : (
-          <p className="col-1"></p>
+          <p className='col-1'></p>
         )}
       </div>
     </div>
